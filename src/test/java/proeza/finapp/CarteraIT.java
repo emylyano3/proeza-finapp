69a4484d86ba6a126dafc996557cd262222a8da5
emylyano3 <emiliano.schiano@gmail.com>
2021-05-08 15:39:46 -0300

    Test basico de consulta (Spring integration)

---
+++ src/test/java/proeza/finapp/CarteraIT.java
@@ -0,0 +1,38 @@
+package proeza.finapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class CarteraIT {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Test
+    public void find () throws Exception {
+        this.mockMvc
+                .perform(get("/api/cartera/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.broker.codigo", is("IOL")));
+    }
+
+    public static final MediaType APPLICATION_JSON = new MediaType(
+            MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype());
+}
